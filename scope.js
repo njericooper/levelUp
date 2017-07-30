eafd54880de6a658a45c12d27443640356421e4f
njericooper <njericooper@gmail.com>
2017-07-30 13:14:35 -0500

    Update

---
+++ scope.js
@@ -0,0 +1,9 @@
+var a = 1-;
+var b = 10;
+function showVars(){
+  var a = 20;
+  b= 20 ;
+  return "Local variable 'a' = " + a +  "\nGlobal variable 'b' =" + b;
+        }
+        var message = showVars();
+        alert 
