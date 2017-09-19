6dc41098f2ac5bd32effca13fea3c8819d60d9c4
njericooper <njericooper@gmail.com>
2017-09-18 22:27:10 -0500

    update

---
+++ settingStyle.js
@@ -0,0 +1,11 @@
+function toggle(){
+  var myElement = document.getElementById("id1");
+  if (myElement.style.backgroundColor == 'red') {
+    myElement.style.backgroundColor = 'yellow';
+    myElement.style.color = 'black';
+  } else {
+    myElement.style.backgroundColor = 'red';
+    myElement.style.color = 'white';
+  }
+
+}
